2a489e99bbe69c16b6c475c03baacea05dcda818
Martyg <martinpaulgray@yahoo.co.uk>
2021-05-05 09:29:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+
+import React, {useState, useEffect} from 'react';
+import Article from './components/Article.js';
+import RedditLogo from './reddit-logo.png';
+
+function App() {
+
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('coding');
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/"+ subreddit +"/.json"). then (res => {
+      if (res.status != 200) {
+        console.log("Sorry something went wrong!");
+        return;
+      }
+      res.json().then(data => {
+        if (data != null) {
+          setArticles(data.data.children);}
+      })
+    })
+  }, [subreddit]);
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+       
+          <img id="logo" src={RedditLogo} alt="Reddit Logo" />
+          <h1>Reddit Reccy</h1>
+       
+        <aside>
+          Seach for Subreddit<input type='text' className="input" value={subreddit} 
+          onChange={e => setSubreddit(e.target.value)} />
+        </aside>
+      </header>
+      <main>
+        <section className="postTitles">
+          <ul>
+            
+              <button className='btn' name='Python' value='Python' onClick={e => setSubreddit(e.target.value)}> 
+              Python
+              </button>
+
+              <button className='btn' name='JavaScript' value='JavaScript' onClick={e => setSubreddit(e.target.value)}> 
+              Javascript
+              </button>
+
+              <button className='btn' name='Comics' value='Comics' onClick={e => setSubreddit(e.target.value)}> 
+              Comics
+              </button>
+
+              <button className='btn' name='Netflix' value='Netflix' onClick={e => setSubreddit(e.target.value)}> 
+              Netflix
+              </button>
+
+              <button className='btn' name='Roblox' value='Roblox' onClick={e => setSubreddit(e.target.value)}> 
+              Roblox
+              </button>
+
+              <button className='btn' name='iphone' value='iphone' onClick={e => setSubreddit(e.target.value)}> 
+              iphone
+              </button>
+  
+          </ul>
+        </section>
+
+        <section>
+   
+          <div className="articles">
+          {
+            (articles != null) ? articles.map((article, index) => 
+            <Article key={index} article={article.data} />) : ""
+          }
+     
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Article.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+//truncatetext
+
+function truncateText(text, limit) {
+	const shortened = text.indexOf(' ', limit);
+	if(shortened == -1) return text;
+	return text.substring(0, shortened);
+}
+
+
+function Article(props) {
+//checking images not working to show reddit logo if no image
+
+	let image = props.article.url
+
+	return(
+
+		<article>
+			<h3>{props.article.title}</h3>
+			<img src={image} alt=" "/>
+			<p>{truncateText(props.article.selftext, 100)}</p>
+			<a href={"https://reddit.com" + props.article.permalink} target='_blank' class="btn">
+				Go to Reddit link
+			</a>
+			<hr>
+			</hr>
+			<span class="author">Author: {props.article.author} &nbsp; &nbsp; Comments: {props.article.num_comments}</span>
+			<span class="score">Score: {props.article.score}</span>
+		
+
+		</article>
+	)
+}
+
+export default Article;
